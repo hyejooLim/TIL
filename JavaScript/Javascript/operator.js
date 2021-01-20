1cdf0ca2183ad8da937ecdb01c705bfadae88d74
hyejooLim <71072930+hyejooLim@users.noreply.github.com>
2021-01-20 12:57:43 +0900

    [04] Javascript

--- JavaScript/Javascript/operator.js
+++ JavaScript/Javascript/operator.js
@@ -1,6 +1,6 @@
 // 1. String concatenation
 console.log('my ' + 'cat');
-console.log('1' + 2); //숫자가 문자열로 변환 
+console.log('1' + 2); // 숫자가 문자열로 변환 
 console.log(`string literals: 1 + 2 = ${1 + 2}`);
 
 // 2. Numeric operators
@@ -9,7 +9,7 @@ console.log(1 - 1); // substract
 console.log(2 * 2); // multiply
 console.log(10 / 5); // divide
 console.log(5 % 2); // remainder
-console.log(2 ** 3); //exponentiation
+console.log(2 ** 3); // exponentiation
 
 // 3. Increment and decrement operators
 let counter = 2;
@@ -31,12 +31,12 @@ x *= y; // x = x * y;
 x /= y; // x = x / y;
 
 // 5. Comparison operators
-console.log(10 < 6); //less than
+console.log(10 < 6); // less than
 console.log(10 <= 6); // less than or equal
 console.log(10 > 6); // greater than
 console.log(10 >= 6); // greater than or equal
 
-// 6. Logical operators : || (or), && (and), ! (not)
+// 6. Logical operators: || (or), && (and), ! (not)
 const value1 = false;
 const value2 = 4 < 2;
 
