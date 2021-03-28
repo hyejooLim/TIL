3d65051e6497873bdd9aef608c17fcae2cea4b3b
hyejooLim <lhyejoo@naver.com>
2021-03-29 00:31:20 +0900

    delete Games folder

--- React/Games/client.jsx
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-const WordRelay = require('./끝말잇기/WordRelay');
-const NumberBaseball = require('./숫자야구/NumberBaseball');
-const ResponseCheck = require('./반응속도체크/ResponseCheck');
-
-ReactDom.render(<ResponseCheck />, document.querySelector('#root'));
