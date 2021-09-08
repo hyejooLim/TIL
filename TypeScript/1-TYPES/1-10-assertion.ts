65e3ea302452e39cc6b749c813d8b309fc81461e
hyejooLim <lhyejoo@naver.com>
2021-09-08 22:32:15 +0900

    [1-10] Assertion

--- TypeScript/1-TYPES/1-10-assertion.ts
+++ TypeScript/1-TYPES/1-10-assertion.ts
@@ -9,13 +9,13 @@
   const result = jsStrFunc();
   console.log((result as string).length); // 5
   console.log((<string>result).length); // 5
-
+ 
   const wrong: any = 5;
-  console.log((wrong as Array<number>).push(1)); // 👿
+  console.log((wrong as Array<number>).push(1)); // 👿 error
 
   function findNumbers(): number[] | undefined {
     return undefined;
   }
   const numbers = findNumbers();
-  numbers!.push(1); // ! -> numbers에 값이 있다고 장담
+  numbers!.push(1); // ! -> numbers에 값이 있다고 장담 error
 }
