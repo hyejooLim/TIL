2297e1b806493e0dfd47ff0f6fb7bb745519c2b4
hyejooLim <lhyejoo@naver.com>
2021-09-12 23:11:24 +0900

    loading quiz

--- TypeScript/2-TYPES-QUIZ/loading.ts
+++ TypeScript/2-TYPES-QUIZ/loading.ts
@@ -20,6 +20,16 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(resourceLoadState: ResourceLoadState) {
+    if (resourceLoadState.state === 'loading') {
+      console.log('👀 loading...');
+    } else if (resourceLoadState.state === 'success') {
+      console.log(`😃 ${resourceLoadState.response.body}`);
+    } else if (resourceLoadState.state === 'fail') {
+      console.log(`😱 ${resourceLoadState.reason}`);
+    }
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
