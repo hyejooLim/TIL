84caac9d2b39c364d86446fd3e153dc767a8563a
hyejooLim <lhyejoo@naver.com>
2021-09-12 23:11:10 +0900

    direction quiz

--- TypeScript/2-TYPES-QUIZ/game.ts
+++ TypeScript/2-TYPES-QUIZ/game.ts
@@ -1,12 +1,38 @@
 /**
  * Let's make a game 🕹
  */
-console.log(position); // { x: 0, y: 0}
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+const position = {
+  x: 0,
+  y: 0,
+};
+
+function move(direction: Direction) {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw new Error('Wrong Direction!');
+  }
+}
+
+console.log(position); // { x: 0, y: 0 }
 move('up');
-console.log(position); // { x: 0, y: 1}
+console.log(position); // { x: 0, y: 1 }
 move('down');
-console.log(position); // { x: 0, y: 0}
+console.log(position); // { x: 0, y: 0 }
 move('left');
-console.log(position); // { x: -1, y: 0}
+console.log(position); // { x: -1, y: 0 }
 move('right');
-console.log(position); // { x: 0, y: 0}
+console.log(position); // { x: 0, y: 0 }
